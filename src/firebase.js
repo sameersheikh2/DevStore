c9f34e93743902b179aabbb773fa0c651738928e
sameersheikh2 <sameersheikh819@gmail.com>
2025-09-22 18:53:36 +0530

    --allow-empty

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import fbConfig from "../.env";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZup1FoYyA24AOGAlvV5VvChjr02atjac",
-  authDomain: "small-business-9072a.firebaseapp.com",
-  projectId: "small-business-9072a",
-  storageBucket: "small-business-9072a.firebasestorage.app",
-  messagingSenderId: "138824203196",
-  appId: "1:138824203196:web:344d6b6f941c44d1fc49b9",
+  apiKey: fbConfig.apiKey,
+  authDomain: fbConfig.authDomain,
+  projectId: fbConfig.projectId,
+  storageBucket: fbConfig.storageBucket,
+  messagingSenderId: fbConfig.messagingSenderId,
+  appId: fbConfig.appId,
 };
 
 // Initialize Firebase
